5845f5ecf00b5fe77bc9c992875a3948b938a7d5
gilbertomedeirox <gilbertom3deiros@gmail.com>
2026-04-03 18:21:46 +0000

    Adiciona lint check e fix

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
 function Home() {
-    return <h1>Te quiero. Se vc me ama, me da um beijinho!
-        - de Gil 
-        - para Michael </h1>
-        
-
-    
-
+  return (
+    <h1>
+      Te quiero. Se vc me ama, me da um beijinho! - de Gil - para Michael{" "}
+    </h1>
+  );
 }
 
 export default Home;
